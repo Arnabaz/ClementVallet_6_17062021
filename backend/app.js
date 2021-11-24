dbbd507d676812fd957b16728e93e800a55b1e17
Clement Vallet <clement.vallet@gmail.com>
2021-11-24 13:34:25 +0100

    feat: installing and congifuring Node server with Express

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+// Import des modules et packages
+const express = require("express"); // Import de Express.js
+
+// Création de l'application Express.js
+const app = express();
+
+// Export de l'application
+module.exports = app;
