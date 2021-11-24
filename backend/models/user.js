a92af1789b3b9f7ee4381eed5fa71244f573f339
Clement Vallet <clement.vallet@gmail.com>
2021-11-24 13:57:50 +0100

    feat: installing and configuring mongoose-unique-validator

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,13 @@
 /* --- Définition d'un schéma de données pour l'utilisateur --- */
 const mongoose = require("mongoose"); // Import du module mongoose
+const uniqueValidator = require("mongoose-unique-validator");// Import du module mongoose-unique-validator
 
 const userSchema = new mongoose.Schema ({
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
+userSchema.plugin(uniqueValidator);
 
 // Création d'un modèle et export
 module.exports = mongoose.model("User", userSchema);
